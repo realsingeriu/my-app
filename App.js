38b21d94ce136c6bdd9f7b5d67203ed68077cdec
realsingeriu <sda1613@gmail.com>
2024-01-31 15:24:52 +0900

    날씨 화면 만들기

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.Text}>날씨앱!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "tomato" }}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  container: {},
+  city: {
+    flex: 1.2,
     justifyContent: "center",
+    alignItems: "center",
   },
-  Text: {
-    fontSize: 28,
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
